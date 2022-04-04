3139d40a9fbf3fa9d536e5a09c4984f07f6b047e
RockLeon <34214497+rockleona@users.noreply.github.com>
2022-04-04 14:40:25 +0800

    fix archive mode

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     crawler.downloadData()
     formatter = formatter.Formatter('./output/crawler_data.json')
 
-    if sys.argv[1] == 'archive':
-        formatter.pushToArchive(sys.argv[2])
+    if str(sys.argv[1]) == 'archive':
+        formatter.pushToArchive(str(sys.argv[2]))
         formatter.initCsvFile()
     else:
         formatter.combineCsvFile()
